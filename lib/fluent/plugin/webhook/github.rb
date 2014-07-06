b672db86838de413837c4d52a5e500440f30914c
uu59 <k@uu59.org>
2014-07-06 16:54:44 +0900

    initial commit

---
+++ lib/fluent/plugin/webhook/github.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/webhook/github/version"
+
+module Fluent
+  module Plugin
+    module Webhook
+      module Github
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/fluent/plugin/webhook/github/version.rb
@@ -0,0 +1,9 @@
+module Fluent
+  module Plugin
+    module Webhook
+      module Github
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end

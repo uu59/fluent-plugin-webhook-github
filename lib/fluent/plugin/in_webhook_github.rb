50ac23bec5880d8cec2dac8c787aec04cb97744c
Songmu <y.songmu@gmail.com>
2014-12-22 00:51:30 +0900

    x-signature supoprt

--- lib/fluent/plugin/in_webhook_github.rb
+++ lib/fluent/plugin/in_webhook_github.rb
@@ -3,6 +3,7 @@
 require "thread"
 require "json"
 require 'fluent/process'
+require 'openssl'
 
 module Fluent
   class WebhookGithubInput < Input
@@ -14,6 +15,7 @@ module Fluent
     config_param :bind, :string, :default => "0.0.0.0"
     config_param :port, :integer, :default => 8080
     config_param :mount, :string, :default => "/"
+    config_param :secret, :string, :default => nil
 
     def start
       @thread = Thread.new(&method(:run))
@@ -22,6 +24,8 @@ module Fluent
     def shutdown
       Thread.kill(@thread)
     end
+
+    HMAC_DIGEST = OpenSSL::Digest.new('sha1')
 
     def run
       server = WEBrick::HTTPServer.new(
@@ -32,10 +36,15 @@ module Fluent
       server.mount_proc(@mount) do |req, res|
         begin
           $log.debug req.header
-          payload = JSON.parse(req.body || "{}")
-          event = req.header["x-github-event"].first
-          process(event, payload)
-          res.status = 204
+
+          if verify_signature(req)
+            payload = JSON.parse(req.body || "{}")
+            event = req.header["x-github-event"].first
+            process(event, payload)
+            res.status = 204
+          else
+            res.status = 401
+          end
         rescue => e
           $log.error e.inspect
           $log.error e.backtrace.join("\n")
@@ -45,33 +54,39 @@ module Fluent
       server.start
     end
 
+    def verify_signature(req)
+      return true unless @secret
+      sig = 'sha1='+OpenSSL::HMAC.hexdigest(HMAC_DIGEST, @secret, req.body)
+      sig == req.header["x-hub-signature"].first
+    end
+
     def process(event, payload)
       content = case event
       when "issue", "issue_comment"
         {
-          :url => payload["issue"]["html_url"],
-          :title => payload["issue"]["title"],
-          :user => payload["issue"]["user"]["login"],
-          :body => payload["comment"]["body"],
+          :url   => payload["issue"] && payload["issue"]["html_url"],
+          :title => payload["issue"] && payload["issue"]["title"],
+          :user  => payload["issue"] && payload["issue"]["user"]["login"],
+          :body  => payload["comment"] && payload["comment"]["body"],
         }
       when "pull_request"
         {
-          :url => payload["pull_request"]["html_url"],
-          :title => payload["pull_request"]["title"],
-          :user => payload["pull_request"]["user"]["login"],
-          :body => payload["pull_request"]["body"],
+          :url   => payload["pull_request"] && payload["pull_request"]["html_url"],
+          :title => payload["pull_request"] && payload["pull_request"]["title"],
+          :user  => payload["pull_request"] && payload["pull_request"]["user"]["login"],
+          :body  => payload["pull_request"] && payload["pull_request"]["body"],
         }
       when "pull_request_review_comment"
         {
-          :url => payload["comment"]["html_url"],
-          :title => payload["pull_request"]["title"],
-          :user => payload["comment"]["user"]["login"],
-          :body => payload["comment"]["body"],
+          :url   => payload["comment"] && payload["comment"]["html_url"],
+          :title => payload["pull_request"] && payload["pull_request"]["title"],
+          :user  => payload["comment"] && payload["comment"]["user"]["login"],
+          :body  => payload["comment"] && payload["comment"]["body"],
         }
       end
       if content
         content[:origin] = "github"
-        $log.info "tag: #{@tag.dup}.#{event}, event:#{event}, content:#{content}" 
+        $log.info "tag: #{@tag.dup}.#{event}, event:#{event}, content:#{content}"
         Engine.emit("#{@tag.dup}.#{event}", Engine.now, content) if content
       else
         $log.warn "unknown hook received #{event} #{payload.inspect}"
--- test/plugin/test_in_webhook_github.rb
+++ test/plugin/test_in_webhook_github.rb
@@ -64,6 +64,34 @@ class GithubWebhookInputTest < Test::Unit::TestCase
     end
   end
 
+
+  def test_signature
+    d = create_driver(CONFIG + %[
+      secret secret1234
+    ])
+
+    time = Time.parse("2011-01-02 13:14:15 UTC").to_i
+    Fluent::Engine.now = time
+
+    d.expect_emit "gwebhook.issue", time, {
+      :url    => nil,
+      :title  => nil,
+      :user   => nil,
+      :body   => nil,
+      :origin => 'github',
+    }
+
+    d.run do
+      d.expected_emits.each {|tag, time, record|
+        res = post("/", '{"hoge":"fuga"}', {
+          'x-github-event'  => 'issue',
+          'x-hub-signature' => 'sha1=5ea783ea13c9feef6dbb9c8c805450e2ba1fb0c0',
+        })
+        assert_equal "204", res.code
+      }
+    end
+  end
+
   def post(path, params, header = {})
     http = Net::HTTP.new("127.0.0.1", PORT)
     req = Net::HTTP::Post.new(path, header)
